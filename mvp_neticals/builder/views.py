d0836045822f78d08329415b0e1159dbbfca9714
Harish Jangra <ch.harish.jangra@gmail.com>
2024-09-09 21:36:48 +0100

    setup builder layout

--- mvp_neticals/builder/views.py
+++ mvp_neticals/builder/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "builder/index.html")
--- mvp_neticals/mvp_neticals/settings.py
+++ mvp_neticals/mvp_neticals/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'builder',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mvp_neticals.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +118,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Your project-level static directory
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# MEDIA SETTINGS
+
+MEDIA_URL = '/media/'  # URL used to access the media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Absolute path to store media files
