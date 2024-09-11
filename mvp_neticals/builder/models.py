838f985067263ddb024c8e0c89c42212bcb5437a
Harish Jangra <ch.harish.jangra@gmail.com>
2024-09-11 17:27:01 +0100

    create initial model for pdb storage

--- mvp_neticals/builder/models.py
+++ mvp_neticals/builder/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class PDBFiles(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pdb_id = models.CharField(max_length=4)
+    entry_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(default='')
+
+    def __str__(self) -> str:
+        return f'{self.pdb_id}'
+
--- mvp_neticals/builder/views.py
+++ mvp_neticals/builder/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request) -> HttpResponse:
     return render(request, "builder/index.html")
+
+
